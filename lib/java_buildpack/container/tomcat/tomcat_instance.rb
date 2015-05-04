dedd3abcf1a030dd2a1b445d4c38faad8c219825
Sani Chabi Yo <chabiyosani@gmail.com>
2015-05-04 14:53:31 -0400

    Adding auto configuration for binding a MySQL service

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -37,7 +37,11 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
         download(@version, @uri) { |file| expand file }
-        link_to(@application.root.children, root)
+
+        repackage_portlet_war
+
+        configure_mysql_service
+
         @droplet.additional_libraries << tomcat_datasource_jar if tomcat_datasource_jar.exist?
         @droplet.additional_libraries.link_to web_inf_lib
       end
@@ -54,6 +58,11 @@ module JavaBuildpack
       end
 
       private
+
+      FILTER = /lf-mysqldb/.freeze
+
+      private_constant :FILTER
+
 
       TOMCAT_8 = JavaBuildpack::Util::TokenizedVersion.new('8.0.0').freeze
 
@@ -108,6 +117,80 @@ module JavaBuildpack
       def web_inf_lib
         @droplet.root + 'WEB-INF/lib'
       end
+
+
+       # The war is presented to the buildpack exploded, so we need to repackage it in a war
+      def repackage_portlet_war
+
+        destination = "#{@droplet.sandbox}" + "/deploy/#{@application.details['application_name']}"+ '.war'
+        with_timing "Packaging #{@application.root} to #{destination} " do
+
+          FileUtils.mkdir_p "#{@droplet.sandbox}/deploy"
+          shell "zip -r #{destination}   #{@application.root}  -x '.*' -x '*/.*' "
+        end
+      end
+
+      # In this method we check if the application is bound to a service. If that is the case then we create the portal-ext.properties
+      # and store it in Liferay Portal classes directory.
+
+      def configure_mysql_service
+
+          @logger       = JavaBuildpack::Logging::LoggerFactory.instance.get_logger TomcatInstance
+          service       = @application.services.find_service FILTER
+
+          @logger.info{ "--->Service:#{service} found" }
+
+          if service.to_s ==''
+            @logger.warn{'No lf-mysqldb SERVICE FOUND #{portal_ext_properties_file}'}
+          else
+              @logger.info{ "Configuring MySQL Store " }
+
+              file = "#{@droplet.sandbox}/webapps/ROOT/WEB-INF/classes/portal-ext.properties"
+
+              if File.exist? (file)
+                  @logger.info { "Portal-ext.properties file already exist, so skipping MySQL configuration" }
+              else
+                  with_timing "Creating portal-ext.properties in #{file}" do
+
+                      credentials   = service['credentials']
+
+                      @logger.debug{ "--->credentials:#{credentials} found" }
+
+                      jdbc_url_name = credentials['jdbcUrl']
+                      host_name     = credentials['hostname']
+                      username      = credentials['username']
+                      password      = credentials['password']
+
+                      @logger.debug {"--->  jdbc_url_name:  #{jdbc_url_name} \n"}
+                      @logger.debug {"--->  username:  #{username} \n"}
+                      @logger.debug {"--->  password:  #{password} \n"}
+                      @logger.debug {"--->  host_name:  #{host_name} \n"}
+
+
+                      File.open(file, 'w') do  |file| 
+                        file.puts("#\n")
+                        file.puts("# MySQL\n")
+                        file.puts("#\n")
+
+                        file.puts("jdbc.default.driverClassName=com.mysql.jdbc.Driver\n")
+                        file.puts("jdbc.default.url=" + jdbc_url_name + "\n")
+                        file.puts("jdbc.default.username=" + username + "\n")
+                        file.puts("jdbc.default.password=" + password + "\n")
+
+
+                        file.puts("#\n")
+                        file.puts("# Configuration of the auto deploy folder\n")
+                        file.puts("#\n")
+
+                        file.puts("auto.deploy.dest.dir=${catalina.home}/deploy\n")
+                        file.puts("auto.deploy.dir=${catalina.home}/deploy\n")
+                        file.puts("auto.deploy.deploy.dir=${catalina.home}/deploy\n")
+                      end
+
+                  end # end with_timing
+              end
+        end # End else
+      end # End def
 
     end
 
